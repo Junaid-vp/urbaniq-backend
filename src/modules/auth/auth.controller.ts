c680876601875996ca6ff12f463149ce8c729450
Rihen <rihenww@gmail.com>
2026-06-19 12:57:37 +0530

    chore: test backend flow

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -15,7 +15,7 @@ export const registerUser = async (req: Request, res: Response) => {
     const userExists = await User.findOne({ email });
 
     if (userExists) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: 'User already existss' });
     }
 
     const user = await User.create({
